4822a80f8015381342fcd1fada2d9f15c51c02db
System Administrator <root@amazon.com>
2016-05-04 23:25:49 -0700

    Added meal model

--- server/models/Meal.js
+++ server/models/Meal.js
@@ -4,7 +4,16 @@
 
 var mongoose = require('mongoose');
 var mealSchema = new mongoose.Schema({
-
+    foodId: String,
+    foodName: String,
+    //image
+    chefId: String,
+    chefName: String,
+    price: {type: Number, max:0},
+    spiceLevel: String,
+    cuisine: String,
+    areaId: String,
+    areaName: String
 });
 
 module.exports = mongoose.model('Meal', mealSchema);
