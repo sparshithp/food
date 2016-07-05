8d756a4bd32e5b3d8d5f8ffeb9f2d12e2f3cfd1e
Sparshith <sparsh.pro@gmail.com>
2016-07-04 22:11:31 -0700

    Signup validations

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -10,6 +10,7 @@ var Order = require('../models/Order');
 
 
 exports.login = function(req, res) {
+    console.log(req.body);
     // middle parameter..
     //check err everywhere
     User.findOne({ email: req.body.email }, '+password', function(err, user){
@@ -30,31 +31,83 @@ exports.login = function(req, res) {
 };
 
 exports.signup = function(req, res){
-    console.log(req.body);
-    //check fullname
-    //check email
-    //check password
-    //check phone
-    //check address
-    //check postal code
-    //check city
-    //check existing user
-    var user = new User();
-    user.fullName = req.body.fullName;
-    user.email = req.body.email;
-    user.password = req.body.password;
-    user.phone = req.body.phone;
-    user.address = req.body.address;
-    user.city = req.body.city;
-    user.zipCode = req.body.zipCode;
-    user.balance = 0;
-    user.chefProfile = null;
+    if(!req.body){
+        res.status(400).send({message: 'Required fields missing'});
+    }
+    var fullName = req.body.fullName;
+    var email = req.body.email;
+    var password = req.body.password;
+    var phone = req.body.phone;
+    var address = req.body.address;
+    var city = req.body.city;
+    var zipCode = req.body.zipCode;
+    var balance = 0;
 
-    user.save(function(err) {
+    if(!fullName){
+        res.status(400).send({message: 'Please input full name'});
+        return;
+    }
+    if(!email){
+        res.status(400).send({message: 'Please input valid email'});
+        return;
+    }
+    if(!password){
+        res.status(400).send({message: 'Please input valid password'});
+        return;
+    }
+    if(!phone){
+        res.status(400).send({message: 'Please input valid phone number'});
+        return;
+    }
+    if(!address){
+        res.status(400).send({message: 'Please input valid address'});
+        return;
+    }
+    if(!city){
+        res.status(400).send({message: 'Please input valid city'});
+        return;
+    }
+    if(!zipCode){
+        res.status(400).send({message: 'Please input valid zip code'});
+        return;
+    }
+
+    email = email.toLowerCase();
+    if(!validateEmail(email)){
+        res.status(400).send({message: 'Please enter the correct email.'});
+        return;
+    }
+    if(password.length <8){
+        res.status(400).send({message: 'Password length should be minimum 8 characters.'});
+        return;
+    }
+
+
+    var user = new User();
+    user.fullName = fullName;
+    user.email = email;
+    user.password = password;
+    user.phone = phone;
+    user.address = address;
+    user.city = city;
+    user.zipCode = zipCode;
+    user.balance = 0;
+
+
+    User.findOne({email: email}, function(err, existingUser){
         if(err){
-            res.send({message: "Problem saving"})
+            res.status(400).send({message: 'Network error. Please try again'});
+        } else if(existingUser){
+            res.status(400).send({message: 'User already exists'});
         }else {
-            res.send({token: createToken(req, user)})
+            user.save(function (err) {
+                if (err) {
+                    console.log(err);
+                    res.status(400).send({message: 'Error saving. Please try again'});
+                } else {
+                    res.send({token: createToken(req, user)})
+                }
+            });
         }
     });
 };
@@ -71,6 +124,11 @@ function createToken(req, user) {
         exp: moment().add(14, 'days').unix()
     };
     return jwt.encode(payload, config.TOKEN_SECRET);
+}
+
+function validateEmail(email) {
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
 }
 
 /* other user apis */
